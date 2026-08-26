67d57a1475b817cc7cef45d042e87d1fab0a1b8e
Zoë <zkcrisler@gmail.com>
2026-08-26 16:03:12 -0500

    Update zorg.c

--- zorg.c
+++ zorg.c
@@ -1,22 +1,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-int BANK[256]={};
-float target=2.00;
+int BANK[256]={}, banked=0;
+float SUM=0, target=2.00;
 
 float account() {
   float a=0;
-  for (int i=0; i<sizeof(BANK)/sizeof(BANK[0]); i++) { a+=(1/i);}
+  for (int i=0; i<banked; i++) { a+=(1/BANK[i]);}
   return a;
 }
 
 int main() {
   for (int i=1; i<=512; i++) {
-    if (sum()>=target) {
-      sum-=1/BANK[0]; #downshift BANK
+    if (sum>=target) {
+      sum-=1/BANK[0];
+      for (int j=1; j<sizeof(BANK)/sizeof(BANK[0]); j++) {
+        BANK[j-1]=BANK[j];
+      }
+      banked-=1;
     }
     sum+=1/i;
-    BANK[sizeof(BANK)/sizeof(BANK[0])]=i;
+    BANK[banked]=i;
+    banked++;
   }
   return 1;
 }
